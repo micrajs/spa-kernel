b5e87315aea3fe3386f0f8d77082a3a778a2c990
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-07-18 15:37:18 -0500

    chore: removed unused linting rule

--- data/classes/SPAKernel.tsx
+++ data/classes/SPAKernel.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import type {Location} from 'history';
 import {createRequestFromLocation} from '../utilities/createRequestFromLocation';
 
